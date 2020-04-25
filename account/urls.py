9b5017380485c047fb9ee1f19b79c3b39011751e
Kirill Rasuli <rasulikv@gmail.com>
2020-04-25 15:30:50 +0300

    Registration system and user profiles added

--- account/urls.py
+++ account/urls.py
@@ -13,8 +13,10 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     # alternative way to include authentication views
     # path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
 
