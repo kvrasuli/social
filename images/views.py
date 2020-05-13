efe977d8208579f32749dbad3eb7fb5e325a2188
Kirill Rasuli <rasulikv@gmail.com>
2020-05-13 20:29:30 +0300

    Form, view and template for saving pictures has been added

--- images/views.py
+++ images/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        # форма отправлена
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            # данные формы валидны
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            # добавляем пользователя к созданному объекту
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+            # перенаправляем пользователя на страницу сохраненного изображения
+            return redirect(new_item.get_absolute_url())
+    else:
+        # заполняем форму данными из get-запроса
+        form = ImageCreateForm(data=request.GET)
+    return render(request,'images/image/create.html', {'section': 'images', 'form': form})
